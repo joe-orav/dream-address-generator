9bcda1e498f6b3d1f447497ea24f31ecc203051a
Joseph Oravbiere <josepho.dev@gmail.com>
2020-08-06 23:34:09 -0400

    Remove report function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./style.scss";
 import { fetchCodes, setNewCode } from "./codes";
-import { displayReportModal, hideReportModal, sendReport } from "./report";
 
 window.onload = () => {
   fetchCodes();
@@ -8,12 +7,3 @@ window.onload = () => {
 
 let generateButton = document.querySelector(".generate-btn");
 generateButton.addEventListener("click", setNewCode);
-
-let reportButton = document.querySelector(".report-btn");
-reportButton.addEventListener("click", displayReportModal);
-
-let denyReportButton = document.querySelector("button.deny");
-denyReportButton.addEventListener("click", hideReportModal);
-
-let confirmReportButton = document.querySelector("button.confirm");
-confirmReportButton.addEventListener("click", sendReport);
