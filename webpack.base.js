22579b862ece549339c6db08f9b644c206c0411c
Joseph Oravbiere <josepho.dev@gmail.com>
2020-08-11 11:51:46 -0400

    Re-organize files; Switch to ejs template

--- webpack.base.js
+++ webpack.base.js
@@ -17,18 +17,31 @@ module.exports = {
           options: {
             name: "[name].[ext]",
             outputPath: "imgs",
+            esModule: false
           },
         },
-      },
-      {
-        test: /\.(html)$/,
-        use: ["html-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html",
+      template: "./src/index.ejs",
+      favicon: "./src/favicon.png",
+      hash: true,
+      templateParameters: {
+        siteTitle: "Dream Address Generator",
+        buttonLabel: "Generate",
+        disclaimer: "Disclaimer: Due to the ability to update dream island addresses, there is the possiblity that a code you get may no longer be valid.",
+        devName: "Joseph Oravbiere",
+        portfolioLink: "https://josephoravbiere.com"
+      },
+      meta: {
+        charset: "UTF-8",
+        viewport: "width=device-width, initial-scale=1.0",
+        description:
+          "Make your next dream island visit a surprise by getting a randomly chosen dream island address",
+        'theme-color': '#373089'
+      },
     }),
     new Dotenv({
       systemvars: true,
--- webpack.dev.js
+++ webpack.dev.js
@@ -12,6 +12,6 @@ module.exports = merge(base, {
     ],
   },
   devServer: {
-    contentBase: "./public",
+    contentBase: "./src",
   },
 });
